e63d98a2f2f1a783201dd7910cd569fc0a675cd8
A02-Kris Vega <krisbvega@hpeprint.com>
2018-03-07 15:44:41 +0800

    2018-03-07 Initial run viewing hard coded items

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kris Vega on 10/02/2018.
+//  Copyright © 2018 16k Foods Services Inc. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike","Buy Sheet","Eat Food"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoListTable", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+}
+
