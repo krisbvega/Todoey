90603c1bd9cbf54a60ae4c430f88ad2f973d5d16
A02-Kris Vega <krisbvega@hpeprint.com>
2018-09-06 17:45:22 +0800

    update as of 2018-09-06

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kris Vega on 19/07/2018.
+//  Copyright © 2018 16k Foods Services Inc. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
